597bf49519e7e6cd4e32608764944794b1db3eeb
luckymaks <maksimenko.maksim.2310@gmail.com>
2019-10-11 17:01:55 +0300

    Added admin page with ability to edit entries.

--- src/App.js
+++ src/App.js
@@ -6,34 +6,54 @@ import { routes } from './routes';
 import { AdminPage } from './scenes/admin/admin';
 import { products } from './data/products';
 
-const getProducts = async () => products;
+const getProducts = async () => new Promise(
+  (resolve) => {
+    setTimeout(() => resolve(products), 1000)
+  }
+);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       products: [],
-    }
+      loading: true,
+    };
+    this.updateProduct = this.updateProduct.bind(this);
   };
 
   async componentDidMount() {
     const prods = await getProducts(); 
     this.setState({
       products: prods,
-    })
+      loading: false,
+    });
   };
+
+  updateProduct(newProduct) {
+    this.setState({
+        products: this.state.products.map((oldProduct) => {
+            if(oldProduct.id === newProduct.id) {
+                return newProduct;
+            }
+            return oldProduct;
+        }),
+
+    })
+  }
+
   render() {
+    if(this.state.loading) {
+      return <h1>Please wait...</h1>
+    }
     return (
       <div className="App">
           <p>
             <Link to={routes.admin}>Admin</Link>
           </p>
         <Route
-          exact
           path={routes.admin}
-          render={
-            (renderProps) => (<AdminPage productList={this.state.products} {...renderProps} />)
-          }
+          render={(renderProps) => (<AdminPage productList={this.state.products} updateProduct={this.updateProduct} {...renderProps} />)}
         />
       </div>
     );
--- src/scenes/admin/admin.js
+++ src/scenes/admin/admin.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import { arrayOf } from 'prop-types';
-
+import { Route } from 'react-router-dom';
+import styled from 'styled-components';
 import { productPropTypes } from '../../common/propTypes';
 import { ProductLink } from '../../components/ProductLink/ProductLink';
+import { routes } from '../../routes';
+import { ProductContainer } from '../../components/ProductContainer/ProductContainer';
 
-export const AdminPage = ({ productList }) => (
-    <div>
-        {productList.map(({id, title}) => <ProductLink key={id} id={id} title={title} />)}
-    </div>
-);
+export class AdminPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.onDeleted = this.onDeleted.bind(this);
+    }
+    
+    onDeleted(id) {
+        console.log(id);
+        
+    }
 
-AdminPage.prototype = {
+    render() {
+        const { productList, match, updateProduct } = this.props;
+        return(
+            <div>
+                <Route
+                    path={match.path}
+                    exact
+                    render={
+                        () => productList.map(({id, title}) => {
+                            return (
+                                <div>
+                                    <ProductLink
+                                        key={id}
+                                        id={id}
+                                        title={title}
+                                    />
+                                </div>
+                            )
+                        }
+                        )
+                    }
+                />
+                <Route
+                    path={routes.adminProduct}
+                    render={(renderProps) => <ProductContainer productList={ productList } updateProduct={updateProduct} {...renderProps} />}
+                />
+            </div>
+        )
+    }
+}
+
+AdminPage.protoType = {
     productList: arrayOf(productPropTypes).isRequired
 }
